d0fa035e6d9f10fd0b3532fa3c44ec5773da8a20
mehiwo617 <astaroth.meffi.me617@gmail.com>
2021-04-01 17:52:07 +0900

    音を鳴らしてスコア追加まで

---
+++ Assets/script/GameDirector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject timerText;
+    GameObject scoreText;
+
+    float time = 60.0f;
+    int score = 0;
+
+    void Start()
+    {
+        this.timerText = GameObject.Find("Time");
+        this.scoreText = GameObject.Find("Score");
+    }
+
+    void Update()
+    {
+        /* 時間操作 */
+        this.time -= Time.deltaTime;
+        this.timerText.GetComponent<Text>().text = this.time.ToString("F1");
+
+        /*スコア操作*/
+        this.scoreText.GetComponent<Text>().text = "Score " + this.score.ToString();
+    }
+
+
+    public void GetTreasure()
+    {
+        this.score += 50;
+    }
+}
--- Assets/script/panda.cs
+++ Assets/script/panda.cs
@@ -9,12 +9,18 @@ public class panda : MonoBehaviour
     private float jumpPower = 150f;
     private Animator anim;
     private Rigidbody rigid;
-    // Start is called before the first frame update
+    GameObject director;
+
+    /* 宝物見つけたときの効果音よう */
+    public AudioClip treasureSE;
+    AudioSource aud;
 
     void Start()
     {
+        this.director = GameObject.Find("GameDirector");
         anim = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody>();
+        aud = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,34 +28,60 @@ public class panda : MonoBehaviour
     {
         anim.SetBool("walking", false);
         // 左に移動
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A))
         {
-            this.transform.Translate(0.0f, 0.0f, -0.1f);
+            this.transform.Translate(0.0f, 0.0f, -0.05f);
             anim.SetBool("walking", true);
         }
         // 右に移動
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D))
         {
-            this.transform.Translate(0.0f, 0.0f, 0.1f);
+            this.transform.Translate(0.0f, 0.0f, 0.05f);
             anim.SetBool("walking", true);
         }
         // 前に移動
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.W))
         {
-            this.transform.Translate(-0.1f, 0.0f, 0.0f);
+            this.transform.Translate(-0.05f, 0.0f, 0.0f);
             anim.SetBool("walking", true);
         }
         // 後ろに移動
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.S))
         {
-            this.transform.Translate(0.1f, 0.0f, 0.0f);
+            this.transform.Translate(0.05f, 0.0f, 0.0f);
             anim.SetBool("walking", true);
         }
+
+        // 右回転
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(0.0f, 0.5f, 0.0f);
+        }
+
+        // 左回転
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(0.0f, -0.5f, 0.0f);
+        }
+
 
         // ジャンプする
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rigid.AddForce(transform.up * this.jumpPower);
+        }
+    }
+
+    /* 衝突操作 */
+    private void OnTriggerEnter(Collider collider)
+    {
+        /* 衝突したのが宝物だった場合のみ */
+        if (collider.CompareTag("Treasure"))
+        {
+            this.aud.PlayOneShot(this.treasureSE);
+            this.director.GetComponent<GameDirector>().GetTreasure();
+            Debug.Log("Catch!!!!");
+            Destroy(collider.gameObject);
         }
     }
 }
